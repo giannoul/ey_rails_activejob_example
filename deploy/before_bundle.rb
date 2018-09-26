7bb16b9838e9c38e8fabb05e47d871ddd2242c49
igiannoulas@LubuntuVM <igiannoulas@engineyard.com>
2018-09-26 14:36:14 +0300

    Testing deploy hook for #142643

--- deploy/before_bundle.rb
+++ deploy/before_bundle.rb
@@ -7,4 +7,7 @@ run "mv #{config.release_path}/config/#{config.environment_name}_credentials.yml
 
 #delete all the %environment%_credentials.yml.enc where %environment% is not the current environment being deployed
 run "echo 'Delete all *_credentials.yml.enc files under #{config.release_path}/config/ except credentials.yml.enc' "
-run "find #{config.release_path}/config/ -name '*_credentials.yml.enc'"
+run "find #{config.release_path}/config/ -name '*_credentials.yml.enc' -delete"
+
+#check the results of the deletion of the unsused %environment%_credentials.yml.enc files
+run "ls -lhart #{config.release_path}/config/ | grep credentials.yml.enc"
