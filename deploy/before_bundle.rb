1df1c875d865384cecc3a4771fc58a8c2abe4498
igiannoulas@LubuntuVM <igiannoulas@engineyard.com>
2018-09-26 14:33:08 +0300

    Testing deploy hook for #142643

--- deploy/before_bundle.rb
+++ deploy/before_bundle.rb
@@ -1,3 +1,6 @@
+#show the existing %environment%_credentials.yml.enc files
+run "ls -lhart #{config.release_path}/config/ | grep credentials.yml.enc"
+
 #move the %environment%_credentials.yml.enc to credentials.yml.enc for the current environment
 run "echo 'Move #{config.release_path}/config/#{config.environment_name}_credentials.yml.enc to #{config.release_path}/config/credentials.yml.enc' "
 run "mv #{config.release_path}/config/#{config.environment_name}_credentials.yml.enc #{config.release_path}/config/credentials.yml.enc"
